bee1ebc96baa2b456d62eb31f07ae2dd48999c84
shlok13-cell <shlokyadav1974@gmail.com>
2026-03-29 19:51:11 +0530

    Time: 260 ms (67.45%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
-select max(salary) as SecondHighestSalary
- from employee
-where salary <(select max(salary) from employee)
+SELECT MAX(salary) AS SecondHighestSalary
+FROM employee
+WHERE salary < (SELECT MAX(salary) FROM employee);
