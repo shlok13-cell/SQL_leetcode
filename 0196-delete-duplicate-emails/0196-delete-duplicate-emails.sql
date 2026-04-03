86ef9ce0044e9a173423eb65bd1f05e5212ebda5
shlok13-cell <shlokyadav1974@gmail.com>
2026-04-03 09:31:39 +0530

    Time: 840 ms (45.27%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,5 @@
-delete person1
-from person as person1
-join person as person2
-on person1.email=person2.email
-and person1.id>person2.id
+DELETE p1
+FROM Person p1
+JOIN Person p2
+ON p1.email = p2.email
+AND p1.id > p2.id;
