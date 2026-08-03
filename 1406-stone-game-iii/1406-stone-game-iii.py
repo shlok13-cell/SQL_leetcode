356b23f6707907bd5c39e445778f0f9de7f05eb9
Shlok Yadav <shlokyadav1974@gmail.com>
2026-08-03 20:20:37 +0530

    Time: 640 ms (71.76%), Space: 23.7 MB (98.21%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        dp = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            take = 0
+            dp[i] = float("-inf")
+            for j in range(i, min(i + 3, n)):
+                take += stoneValue[j]
+                dp[i] = max(dp[i], take - dp[j + 1])
+
+        if dp[0] > 0:
+            return "Alice"
+        elif dp[0] < 0:
+            return "Bob"
+        else:
+            return "Tie"
